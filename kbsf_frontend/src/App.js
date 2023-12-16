fe9449c9a0eca2805ca2b5d0b253bad085dd772d
mHaines331 <mhaines9219@gmail.com>
2023-12-15 18:39:42 -0800

    link back and front

--- kbsf_frontend/src/App.js
+++ kbsf_frontend/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/api/todos/')
+      .then((res) => {
+        console.log(res.data);
+        setTodoList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {todoList.map((t) => {
+            return <div>{t.title}</div>;
+          })}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
