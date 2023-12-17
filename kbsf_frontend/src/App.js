507b5d1545ebf93c700b7e56671cacadec52020e
mHaines331 <mhaines9219@gmail.com>
2023-12-16 20:24:19 -0800

    navbar

--- kbsf_frontend/src/App.js
+++ kbsf_frontend/src/App.js
@@ -2,30 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import API from './API';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  // const [todoList, setTodoList] = useState([]);
+  // const [title, setTitle] = useState('');
+  // const [description, setDescription] = useState('');
+  // const [completed, setCompleted] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get('http://localhost:8000/api/todos/')
-      .then((res) => {
-        console.log(res.data);
-        setTodoList(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(
+  //   () =>
+  //     // {
+  //     //   axios
+  //     //     .get('http://localhost:8000/api/todos/')
+  //     //     .then((res) => {
+  //     //       console.log(res.data);
+  //     //       setTodoList(res.data);
+  //     //     })
+  //     //     .catch((err) => {
+  //     //       console.log(err);
+  //     //     });
+  //     // }
+  //     refreshTodos,
+  //   []
+  // );
 
+  // const refreshTodos = () => {
+  //   API.get('/')
+  //     .then((res) => {
+  //       setTodoList(res.data);
+  //     })
+  //     .catch(console.err);
+  // };
+
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
+  //   let todo = { title, description, completed };
+  //   API.post('/', todo).then(() => refreshTodos());
+  // };
   return (
     <div className="App">
       <div>
-        <p>
-          {todoList.map((t) => {
-            return <div>{t.title}</div>;
-          })}
-        </p>
+        <Navbar />
       </div>
     </div>
   );
