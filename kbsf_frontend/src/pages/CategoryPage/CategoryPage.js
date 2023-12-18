ade4f5ca2abd128b00907db4aeda0ffbe6d48fa8
mHaines331 <mhaines9219@gmail.com>
2023-12-17 18:29:52 -0800

    setting up rendering

--- kbsf_frontend/src/pages/CategoryPage/CategoryPage.js
+++ kbsf_frontend/src/pages/CategoryPage/CategoryPage.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Footer from '../../components/Footer/Footer';
 import Navbar from '../../components/Navbar/Navbar';
+import API from '../../API.js';
 
 export default function CategoryPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    API.get('/')
+      .then((res) => {
+        setProducts(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.error('Error fetching data: ', error));
+  }, []);
+
   return (
-    <>
-      <div>category</div>
-    </>
+    <div>
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            {/* Render other product details */}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
