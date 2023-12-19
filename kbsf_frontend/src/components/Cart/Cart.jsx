ea31c11580b2ec6490bf6b35ac306a239e2a9bb7
mHaines331 <mhaines9219@gmail.com>
2023-12-19 14:15:30 -0800

    cart shows contents

--- kbsf_frontend/src/components/Cart/Cart.jsx
+++ kbsf_frontend/src/components/Cart/Cart.jsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
 import ShoppingCartIcon from '../../assets/cart_icon.png';
+import './Cart.css';
+import { Link } from 'react-router-dom';
 
 export default function Cart({ cartItems }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      <img src={ShoppingCartIcon} alt="Cart" />
-      <span>{cartItems.length - 1}</span>
-    </div>
+    <>
+      <div>
+        <img
+          src={ShoppingCartIcon}
+          alt="Cart"
+          onClick={() => setIsOpen(!isOpen)}
+        />
+        <span>{cartItems.length}</span>
+        {isOpen && (
+          <div className="cart-container">
+            {cartItems.map((item, index) => (
+              <>
+                <div key={index}>{item.title}</div>
+                <button>Delete</button>
+              </>
+            ))}
+            <Link to="/checkout">CHECKOUT</Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
